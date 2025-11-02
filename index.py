537527165c897f9d71bbce1ed70212ca3d79c3bf
Zain-ul-abidin9702 <zainulabidin970946@gmail.com>
2025-11-02 10:19:02 +0500

    proper functions with structure and calling

--- index.py
+++ index.py
@@ -2,9 +2,22 @@ print("This is my portfolio website")
 print("Nice test")
 
 
-def printHello():
-    print("Hello world from the function")
+users = []
 
-def getAllUsera():
-    print("=====================");
-printHello();
+def printUsers():
+    print("Print users : ",users);
+
+def getAllUsers():
+    return users
+
+def createUser(user):
+    users.append(user)
+
+print("Creating User");
+createUser({"name":"zain","age":22})
+
+printUsers();
+print("Get All Users : ",getAllUsers());
+
+print("Creating User");
+createUser({"name":"ehsan","age":45})
